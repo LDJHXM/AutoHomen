9f8d83f61db09b4d0fbe41783a9e9fa1a222675e
sunshineholiday <hx_Android2014@126.com>
2016-11-17 19:50:42 +0800

    我的界面布局

--- app/src/main/java/com/l000phone/fragment/Mine.java
+++ app/src/main/java/com/l000phone/fragment/Mine.java
@@ -1,5 +1,7 @@
 package com.l000phone.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,8 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.l000phone.autohomen.MainActivity;
+import com.l000phone.autohomen.MyLoginActivity;
+import com.l000phone.autohomen.MySettingActivity;
 import com.l000phone.autohomen.R;
 
 /**
@@ -16,7 +23,7 @@ import com.l000phone.autohomen.R;
  */
 
 public class Mine extends Fragment {
-
+    private View views;
     private Button myLogin;
     private Button myRegist;
     private TextView myRecipe;
@@ -28,6 +35,12 @@ public class Mine extends Fragment {
     private TextView myDownload;
     private TextView myCollection;
     private TextView myOrder;
+    private TextView myName;
+    private ImageView mySetting;
+
+    //判断是否登录的标记值
+    private boolean flag = true;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
 
@@ -38,26 +51,69 @@ public class Mine extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.my_fragment,null);
+       if(flag) {
+          views = inflater.inflate(R.layout.fragment_my, null);
+           //界面控件实例初始化
+           intiWidge();
+           return views;
 
-        //界面控件实例初始化
-        intiWidge();
-        return view;
+       }else{
+          views = inflater.inflate(R.layout.fragment_myright,null);
+           //界面控件实例的初始化
+           initRightWdger();
+           //界面空间实例对应的相关跳转
+//            jumpRightWidge();
+           return views;
+       }
+
+
+
     }
-
-
-
-
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        
+
        super.onActivityCreated(savedInstanceState);
     }
 
+//    private void jumpRightWidge() {
+//    }
+
+
     /**
-     * 界面实例初始化
+     * 登录，界面实例初始化
+     */
+    private void initRightWdger() {
+        myName =(TextView)views.findViewById(R.id.tv_myright_user) ;
+        mySetting = (ImageView)views.findViewById(R.id.img_myright_setting) ;
+        myRecipe = (TextView) views.findViewById(R.id.tv_myright_user);
+        myRecipe = (TextView) views.findViewById(R.id.tv_myright_menu);
+        myDraft  =  (TextView) views.findViewById(R.id.tv_myright_draft);
+        myTopic  =  (TextView) views.findViewById(R.id.tv_myright_topic);
+        myPutRegist = (TextView) views.findViewById(R.id.tv_myright_recipe);
+        myAction = (TextView) views.findViewById(R.id.tv_myright_action);
+        myMessage = (TextView) views.findViewById(R.id.tv_myright_message);
+        myDownload = (TextView) views.findViewById(R.id.tv_myright_download);
+        myCollection = (TextView) views.findViewById(R.id.tv_myright_collection);
+        myOrder = (TextView) views.findViewById(R.id.tv_myright_order);
+    }
+
+    /**
+     * 未登录，界面实例的初始化
      */
     private void intiWidge() {
-
+        mySetting = (ImageView)views.findViewById(R.id.img_my_setting) ;
+        myLogin = (Button) views.findViewById(R.id.btn_my_login);
+        myRegist = (Button) views.findViewById(R.id.btn_my_regist);
+        myRecipe = (TextView) views.findViewById(R.id.tv_my_menu);
+        myDraft  =  (TextView) views.findViewById(R.id.tv_my_draft);
+        myTopic  =  (TextView) views.findViewById(R.id.tv_my_topic);
+        myPutRegist = (TextView) views.findViewById(R.id.tv_my_recipe);
+        myAction = (TextView) views.findViewById(R.id.tv_my_action);
+        myMessage = (TextView) views.findViewById(R.id.tv_my_message);
+        myDownload = (TextView) views.findViewById(R.id.tv_my_download);
+        myCollection = (TextView) views.findViewById(R.id.tv_my_collection);
+        myOrder = (TextView) views.findViewById(R.id.tv_my_order);
     }
+
+
 }
