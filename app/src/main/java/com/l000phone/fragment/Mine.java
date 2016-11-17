45476f4a0f17768082eb8e45def0fd439160c86f
sunshineholiday <hx_Android2014@126.com>
2016-11-17 16:48:42 +0800

    我的界面布局

--- app/src/main/java/com/l000phone/fragment/Mine.java
+++ app/src/main/java/com/l000phone/fragment/Mine.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.l000phone.autohomen.R;
 
 /**
  * Created by DJ on 2016/11/16.
@@ -13,6 +17,17 @@ import android.view.ViewGroup;
 
 public class Mine extends Fragment {
 
+    private Button myLogin;
+    private Button myRegist;
+    private TextView myRecipe;
+    private TextView myDraft;
+    private TextView myTopic;
+    private TextView myPutRegist;
+    private TextView myAction;
+    private TextView myMessage;
+    private TextView myDownload;
+    private TextView myCollection;
+    private TextView myOrder;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
 
@@ -23,14 +38,26 @@ public class Mine extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.my_fragment,null);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        //界面控件实例初始化
+        intiWidge();
+        return view;
     }
+
+
 
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         
-        super.onActivityCreated(savedInstanceState);
+       super.onActivityCreated(savedInstanceState);
+    }
+
+    /**
+     * 界面实例初始化
+     */
+    private void intiWidge() {
+
     }
 }
